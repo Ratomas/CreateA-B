a49acf12eee22bf3833f608729db435376cca9d3
Ratomas <ratomas79@gmail.com>
2022-01-26 19:23:46 -0600

    Trying to get it working but having issues with downloading the zip file from Curseforge

--- launch.sh
+++ launch.sh
@@ -12,10 +12,16 @@ else
 fi
 
 if ! [[ -f Above+and+Beyond-1.3-Server.zip ]]; then
-	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu SkyFactory_One_Server_*.zip
-	curl -o Above+and+Beyond-1.3-Server.zip https://www.curseforge.com/minecraft/modpacks/create-above-and-beyond/download/3567576/file && unzip -u -o Above+and+Beyond-1.3-Server.zip -d /data
-	chmod +x Install.sh
-	./Install.sh
+	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu Above+and+Beyond-*-Server.zip
+	curl -L -o Above+and+Beyond-1.3-Server.zip https://www.curseforge.com/minecraft/modpacks/create-above-and-beyond/download/3567576 && unzip -u -o Above+and+Beyond-1.3-Server.zip -d /data
+	
+	install_files() {
+		java -jar ${INSTALL_JAR} --installServer > /dev/null 2>&1
+	}
+	
+	echo "Installing Forge and required jars."
+	install_files
+	echo "Done!"
 fi
 
 if [[ -n "$MOTD" ]]; then
@@ -29,6 +35,5 @@ if [[ -n "$OPS" ]]; then
     echo $OPS | awk -v RS=, '{print}' >> ops.txt
 fi
 
-. ./settings.sh
 curl -o log4j2_112-116.xml https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml
 java $JVM_OPTS -XX:MaxPermSize=256M -Dlog4j.configurationFile=log4j2_112-116.xml -jar $SERVER_JAR nogui
